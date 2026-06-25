6fa4886229999cfbcaa0342281307116c5ce5c15
Raghad Alharbi <rgd.alharbi4@gmail.com>
2026-06-25 16:30:43 +0300

    Update hang.py

--- games/hang.py
+++ games/hang.py
@@ -211,3 +211,13 @@ class Hang:
 
 c=Hang()
 c._start()
+
+#ٍReflections
+# 1. What was the most challenging part?
+Integrating the Python game logic with Flask and rendering it dynamically onto the web interface.
+
+# 2. Which concept did you enjoy the most?
+Designing and writing the core game logic and methods within the Object-Oriented structure.
+
+# 3. What would you improve if you had more time?
+Expanding the word vocabulary list and integrating interactive sound effects to enhance the user experience.
